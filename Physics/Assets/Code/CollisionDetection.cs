a9633679e88230f387133421b46a2399e0dc832a
Carlos Sivira <carlos.csivira@gmail.com>
2019-11-07 18:27:00 -0400

    Clase de fisicas

---
+++ Physics/Assets/Code/CollisionDetection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+
+    //Para colisionadores
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Colision");
+    }
+
+    //Para disparadores
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
---
+++ Physics/Assets/Code/ObjectMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    public float speed = 10.00f;
+    private Rigidbody rb;
+    private Vector3 dir;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        dir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+    }
+
+    private void FixedUpdate()
+    {
+        //RbVelocityMovement(dir);
+        //RbForceMovement(dir);
+        RbMovePosition(dir);
+    }
+
+    void RbVelocityMovement(Vector3 direction)
+    {
+        rb.velocity = direction * speed;
+    }
+
+    void RbForceMovement(Vector3 direction)
+    {
+        rb.AddForce(direction * speed);
+    }
+
+    void RbMovePosition(Vector3 direction)
+    {
+        rb.MovePosition(transform.position + (direction * speed * Time.deltaTime));
+    }
+}
